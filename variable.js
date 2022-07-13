986d4e1e1b4c996fbda529cf837c80760d5dd1a1
Adiaz <adiaz@engineer.com>
2022-07-13 09:52:50 +0700

    Jenis Variable

---
+++ variable.js
@@ -0,0 +1,21 @@
+// String
+// huruf = 'a,b,c';
+
+// Number
+// boelat = 1;
+// decimal = 2.9;
+
+// Boolean
+// boolean1 = true;
+// boolean2 = false;
+
+// Deklarasi variable
+// var, let, const
+
+let a = 2;
+let b = 2;
+
+if (a == b){
+    let c = a * b;
+    console.log(c); 
+}
